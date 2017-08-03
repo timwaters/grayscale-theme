5ab109385eedcb968a964f452cd541357fb25690
Tim Waters <chippy2005@gmail.com>
2017-08-03 23:04:53 +0100

    remove debugging var

--- js/grayscale.js
+++ js/grayscale.js
@@ -29,12 +29,12 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-var foo;
+
 $(function() {
 
 
     var map = L.map('map').setView([53.646774, -1.77856], 15);
-foo = map;
+
     L.tileLayer('https://api.mapbox.com/styles/v1/chippy/cirfh4hwx0003h5kprgysft36/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hpcHB5IiwiYSI6IjVRbVFqMWsifQ.7wYEXYi6OBRNDU0roXhb8w', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors. Map tiles from <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 19
